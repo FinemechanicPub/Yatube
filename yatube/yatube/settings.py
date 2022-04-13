64501c3872507492e7973c9f96d8383543741060
Alexander Rubtsov <equester@mail.ru>
2022-04-13 17:11:23 +0000

    Setting for Nginx

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$x6@^iytre-)x3k6x=2-^*^8xzequ95y!8&jotsgse(j!vkwr1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['51.250.98.22', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
